a52113001832e746f190e9e2fba2eb647ab6de3c
Amir Hasan Zaizakarani <43024238+aihara44@users.noreply.github.com>
2024-11-09 20:17:05 +0800

    Delete logistics - Copy directory

--- logistics - Copy/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con = mysqli_connect("localhost", "root", "", "persada") or die ("Cannot connect to database");
-function mres ($con,$data){
-    return mysqli_real_escape_string($con,rtrim(ltrim($data)));
-}
-?>
