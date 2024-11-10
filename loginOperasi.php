a832bf52623e6fc6bf2b7fb94b3d41b3ec95181a
Amir Hasan Zaizakarani <43024238+aihara44@users.noreply.github.com>
2024-11-10 11:51:15 +0800

    Update loginOperasi.php

--- loginOperasi.php
+++ loginOperasi.php
@@ -51,11 +51,11 @@ else if(isset($_POST["btn_password"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Persada Dunya Distribution Center - Operation Department Login Page</title>
     <!-- Bootstrap -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/custom.css">
-    <!-- jQuery -->
-    <script src="../js/jquery-1.12.1.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+      <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>  
+           <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css"> 
   </head>
   <body>
     <div class="container">
