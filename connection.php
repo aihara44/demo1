c0a9c132ade2893429b56851339e8c1cb90a5aa1
Amir Hasan Zaizakarani <43024238+aihara44@users.noreply.github.com>
2024-11-12 23:32:04 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "persada") or die ("Cannot connect to database");
+$con = mysqli_connect("fypbns.mysql.database.azure.com", "fyp_admin", "Amirhasan@990630", "persada") or die ("Cannot connect to database");
 function mres ($con,$data){
     return mysqli_real_escape_string($con,rtrim(ltrim($data)));
 }
